4d43efde56d6feee2178793a8b3c45031993e03e
Melena Suliteanu <melenasuliteanu@gmail.com>
2017-12-19 14:33:26 -0700

    Make working find node function. Transition from letters as keys on node to having a next property that is an object containing all the next nodes

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -16,17 +16,48 @@ describe.only('Trie', function() {
   describe('INSERT', () => {
     it('should add items to the trie', () => {
       trie.insert('fern');
-      expect(trie.root.f.data).to.equal('f');
-      expect(trie.root.f.e.data).to.equal('e');
-      expect(trie.root.f.e.r.n.data).to.equal('n');
+      expect(trie.root.next.f.data).to.equal('f');
+      expect(trie.root.next.f.next.e.data).to.equal('e');
+      expect(trie.root.next.f.next.e.next.r.next.n.data).to.equal('n');
     })
 
     it('should insert multiple words', () => {
       trie.insert('sanseveria');
       trie.insert('cactus');
 
-      expect(trie.root.s.a.data).to.equal('a');
-      expect(trie.root.c.a.c.data).to.equal('c');
+      expect(trie.root.next.s.next.a.data).to.equal('a');
+      expect(trie.root.next.c.next.a.next.c.data).to.equal('c');
+    })
+
+    it('should track how many words it has', () => {
+      trie.insert('ficus');
+      trie.insert('marigold');
+      trie.insert('oregano');
+
+      expect(trie.count).to.equal(3);
+    })
+  })
+
+  describe('SUGGEST', () => {
+    it('should return an array', () => {
+      expect(trie.suggest()).to.deep.equal([]);
+    })
+
+    it('should return an array of possibilities', () => {
+      trie.insert('helleborus');
+      trie.insert('heliotrope');
+      trie.insert('heliconia');
+
+      expect(trie.suggest('hel')).to.equal(['helleborus', 'heliotrope', 'heliconia'])
+    })
+  })
+
+  describe('FINDNODE', () => {
+    it('should find the node of the last letter', function() {
+      trie.insert('aloe');
+
+      expect(trie.findNode('al')).to.equal(trie.root.next.a.next.l);
+
     })
   })
 })
